391cb433000e5aa4f156d5e07a82b73f521eb500
Sandip Rathod <rathodsandip81873@gmail.com>
2026-07-13 10:55:01 +0530

    Add files via upload

---
+++ math.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a,b, sum,sub,multiplication,modulus,division;
+a=7;
+b=9;
+sum=a+b;
+sub=a-b;
+multiplication=a*b;
+modulus=a%b;
+division=a/b;
+cout<<sum;
+cout<<sub;
+cout<<multiplication;
+cout<<modulus;
+cout<<division;
+}
