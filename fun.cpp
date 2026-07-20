25d26b4f501d01434da2f82f544e14a7ebf3e616
Sandip Rathod <rathodsandip81873@gmail.com>
2026-07-20 10:48:10 +0530

    Add files via upload

---
+++ fun.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a,b;
+cout<<"enter num.1:";
+cin>>a;
+cout<<"enter num.2:";
+cin>>b;
+cout<<a+b<<endl;
+cout<<a-b<<endl;
+cout<<a/b<<endl;
+cout<<a%b<<endl;
+}
