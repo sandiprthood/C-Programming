41010e6ebe5d5a523a84b91235283723efebda4b
Sandip Rathod <rathodsandip81873@gmail.com>
2026-07-20 10:54:41 +0530

    Add files via upload

---
+++ circleareacir.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+float r;
+const float pi=3.14;
+
+cout<<"enter radius: ";
+cin>> r;
+cout<<"area="<<pi*r*r<<endl;
+cout<<"circumference="<<2*pi*r;
+}
+
