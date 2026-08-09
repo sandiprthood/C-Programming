42d0233fe2ab2c18f517b09500cfa99dadabe3e6
Sandip Rathod <rathodsandip81873@gmail.com>
2026-08-09 07:16:45 +0000

    change

---
+++ square2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int square(int n){
+    return n*n;
+}
+int main(){
+    int n ;
+    cin>>n;
+
+    
+    cout<<"square"<<square(n)<<endl;
+    return 0;
+}
